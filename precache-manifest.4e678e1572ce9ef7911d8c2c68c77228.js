f6f515e1159003ed42881faaa956b066295280fa
FRustem <lionelmessi@bk.ru>
2019-01-14 11:33:59 +0300

    Add files via upload

---
+++ precache-manifest.4e678e1572ce9ef7911d8c2c68c77228.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a2c8eef43016867f5dbcf1d363e931d",
+    "url": "/static/media/toilet.4a2c8eef.svg"
+  },
+  {
+    "revision": "d8d69a4d2338dda1e595",
+    "url": "/static/css/main.cc0edb1b.chunk.css"
+  },
+  {
+    "revision": "986e6e22ce26e5fd6feb",
+    "url": "/static/js/1.986e6e22.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "44707e76570bf7da710b7dea2f00e7cd",
+    "url": "/static/media/filter.44707e76.svg"
+  },
+  {
+    "revision": "35aeb935b2bc0be23cabb5528a6caa21",
+    "url": "/static/media/escalator.35aeb935.svg"
+  },
+  {
+    "revision": "d8d69a4d2338dda1e595",
+    "url": "/static/js/main.d8d69a4d.chunk.js"
+  },
+  {
+    "revision": "8310a9de57e02fd345f6e3f1b2196fa2",
+    "url": "/static/media/lift.8310a9de.svg"
+  },
+  {
+    "revision": "31891c307de2dbb214c3497641e393ce",
+    "url": "/static/media/parking.31891c30.svg"
+  },
+  {
+    "revision": "9440210e4104dd2a65ce339ed46db0e3",
+    "url": "/static/media/stairs.9440210e.svg"
+  },
+  {
+    "revision": "3989553cc4a59a55f1a22a30057b443f",
+    "url": "/static/media/close-cross.3989553c.svg"
+  },
+  {
+    "revision": "1dd3e8a8abae76161dc44b522b94f03d",
+    "url": "/index.html"
+  }
+];
